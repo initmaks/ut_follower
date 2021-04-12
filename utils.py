rot_dist -> angle diff - fix sign

utils.py
@@ -5,10 +5,13 @@ def lin_dist(point1,point2):
     if type(point2) is not np.ndarray: point2 = np.array(point2)
     return np.linalg.norm(point1-point2)
 
-def rot_dist(a,b):
-    raw_diff = np.abs(a-b)
-    mod_diff = raw_diff % (2*np.pi)
-    return np.pi - np.abs(mod_diff - np.pi)
+def angle_diff(a,b):
+	raw_diff = (a-b+np.pi)
+	diff = raw_diff % (2*np.pi) - np.pi
+	if diff < -np.pi:
+		return diff + 2*np.pi
+	else:
+		return diff
 
 def ranged_rotation_angle(r):
     # makes sure the r is always between -pi and pi